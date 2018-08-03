a9981903680e1dfd3414f4d15c658e8795cc1d04
Denilson Gama <denilson.gama@gamadeveloper.com.br>
2018-08-02 22:43:35 -0300

    tenth commit: Implementação de audiomanager

---
+++ app/src/main/java/br/com/fiap/recursosmultimidia/sons_audiomanager.kt
@@ -0,0 +1,41 @@
+package br.com.fiap.recursosmultimidia
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class Sons_AudioManager : AppCompatActivity() {
+
+    var cow: MediaPlayer? = null
+    var dog: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sons_audiomanager)
+
+        cow = MediaPlayer.create(this, R.raw.cow)
+        dog = MediaPlayer.create(this, R.raw.dog)
+    }
+
+    fun soundCow(view: View) {
+
+        if(dog != null) {
+            dog!!.stop()
+        }
+        cow!!.start()
+    }
+
+    fun soundDog(view: View) {
+
+        if(cow != null) {
+            cow!!.stop()
+        }
+        dog!!.start()
+    }
+}
+
+
+
+
